aff8f2f07899828f878c1db15d5ece1bc6f4ed7a
Andrew <127788127788a@gmail.com>
2024-05-08 13:22:42 +0300

    creating footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -1,5 +1,37 @@
 import "./Footer.css";
 
 const Footer: React.FC = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Math Symphony</h1>
+            <p>
+              Math Symphony - это не просто сайт о математике, это место, где
+              математика становится мелодией.
+            </p>
+            <div className="social-links">
+              <a href="#">
+                <i className="fab fa-telegram"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-github"></i>
+              </a>
+            </div>
+          </div>
+          <div className="col"></div>
+          <div className="col"></div>
+        </div>
+      </div>
+    </footer>
+  );
 };
+
+export { Footer };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/common/Header/Header";
+import { Footer } from "../../components/common/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
