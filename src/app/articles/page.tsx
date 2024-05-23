d5f557db8a603bb29d3358fbadadf371600c0532
Andrew <127788127788a@gmail.com>
2024-05-23 21:51:52 +0300

    changed styles

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,4 +1,4 @@
-import EventList from "../../../components/ArticlesPageComponents/HistoricalArticles/ EventList";
+import EventList from "../../../components/ArticlesPageComponents/HistoricalArticles/EventList";
 import { Magazines } from "../../../components/ArticlesPageComponents/Magazines/Magazines";
 import { RecentDiscoveries } from "../../../components/ArticlesPageComponents/RecentDiscoveries/RecentDiscoveries";
 
