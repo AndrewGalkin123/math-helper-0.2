8b9ff5317b00547b8ffd38f67385d672382fdcf1
Andrew <127788127788a@gmail.com>
2024-07-22 13:48:50 +0300

    editing styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
         <Header />
